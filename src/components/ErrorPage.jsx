9ac6adab9faa048bf81a9f9039e61a3eb3e7f5b9
Matías <matiasagbenitez@gmail.com>
2023-03-30 22:43:03 -0300

    integración de json-server al proyecto + pantalla de error

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+import React from "react";
+
+export const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <div className="space-y-8">
+      <h1 className="text-center text-4xl font-extrabold mt-10 text-blue-900">
+        CRM Clientes
+      </h1>
+      <p className="text-center">Hubo un error</p>
+      <p className="text-center">{error.message}</p>
+    </div>
+  );
+};
---
+++ src/data/clientes.js
@@ -0,0 +1,5 @@
+export const obtenerClientes = async () => {
+    const respuesta = await fetch(import.meta.env.VITE_API_URL);
+    const resultado = await respuesta.json();
+    return resultado;
+}
